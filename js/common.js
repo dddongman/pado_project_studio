dc0c7b5e298a07e91d28cb55c36ec7433ae25812
Dongmin <56422180+dddongman@users.noreply.github.com>
2022-06-13 21:57:30 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+    AOS.init({
+        offset: 200,
+        delay: 0,
+        duration: 1000,
+        easing: 'ease',
+        once: false,
+        mirror: true,
+        anchorPlacement: 'top-bottom'
+      });    
+  
+      AOS.init();
+})
