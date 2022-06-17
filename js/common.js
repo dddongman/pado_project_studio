61d04f20b74b148f6f707ec0fdf7d4421ba60cc2
Dongmin <56422180+dddongman@users.noreply.github.com>
2022-06-17 10:50:09 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,13 +1,11 @@
-$(function() {
-    AOS.init({
-        offset: 200,
-        delay: 0,
-        duration: 1000,
-        easing: 'ease',
-        once: false,
-        mirror: true,
-        anchorPlacement: 'top-bottom'
-      });    
-  
-      AOS.init();
-})
+// $('.chart').easyPieChart({
+//     barColor: '#f7436d',  //차트가 그려질 색
+//     trackColor: '#ededed',  // 차트가 그려지는 트랙의 기본 배경색(chart1 의 회색부분)
+//     scaleColor: '#fff', // 차트 테두리에 그려지는 기준선 (chart2	의 테두리 선)
+//     lineCap: 'round', // 차트 선의 모양 chart1 butt / chart2 round / chart3 square
+//     lineWidth: 10, // 차트 선의 두께
+//     size: 120, // 차트크기
+//     animate: 1000, // 그려지는 시간 
+//     onStart: $.noop,
+//     onStop: $.noop
+//   });
